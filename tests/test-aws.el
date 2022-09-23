86ff5c2e35183e577af7f22e6e717bd3e6893cba
MB <mb@blaster.ai>
2022-09-23 12:57:50 +0300

    Fix test

--- tests/test-aws.el
+++ tests/test-aws.el
@@ -45,7 +45,7 @@
 (defvar-local aws-test-example-ls-s3-output
       '(("2021-12-20" "15:02:04" "cognitect-dev-local") ("2022-05-16" "19:42:42" "compose-segment-db-backup") ("2021-04-13" "14:23:30" "datomic-code-32068047-eb82-4f94-914b-af1f8ce6144e") ("2021-02-05" "14:44:03" "datomic-code-95186d6e-15ef-4eb1-b21e-b2e0b6ddc9da") ("2021-07-06" "00:36:12" "db-example-storage-s3datomic-1f6s5g2cqoyz3") ("2021-02-05" "14:40:26" "db-learn-storagef7f305e7-1y046qjbmg9zd-s3datomic-12oxiz7jr5jnl")))
 
-(describe
+(describe "s3-ls tests"
     (it "check list formatting for aws s3 ls"
       (expect (aws--s3-ls-format aws-test-example-ls) :to-equal aws-test-example-ls-s3-output)))
 
